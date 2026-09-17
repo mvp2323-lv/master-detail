537cba1bd21c3dba4dc18e39c18c5b5ff01846a5
mvp2323-lv <mahoganyvanpelt@gmail.com>
2026-09-17 18:02:08 -0500

    fix spreadsheet path for hybrid hosting

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const CSV_PATH = "iowa-warblers.csv";
+const CSV_PATH = "https://mvp2323-lv.github.io/master-detail/iowa-warblers.csv";
 const BIRD_THUMBNAIL_EXTENSION = ".jpg";
 const FOOD_IMAGE_EXTENSION = ".jpg";
 
