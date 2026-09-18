1a29518b6ae3d9557ff5f78fcdb119f4b1c63049
mvp2323-lv <mahoganyvanpelt@gmail.com>
2026-09-17 19:15:12 -0500

    stop jumping to top of page

--- script.js
+++ script.js
@@ -206,7 +206,7 @@ function renderDetail(bird) {
 
   requestAnimationFrame(() => {
     document.querySelector("#detail-name")?.focus({ preventScroll: true });
-    window.scrollTo({ top: 0, behavior: "auto" });
+    // window.scrollTo({ top: 0, behavior: "auto" });
   });
 }
 
@@ -224,7 +224,7 @@ function closeDetail() {
 
   requestAnimationFrame(() => {
     lastSelectedButton?.focus({ preventScroll: true });
-    window.scrollTo({ top: 0, behavior: "auto" });
+    // window.scrollTo({ top: 0, behavior: "auto" });
   });
 }
 
